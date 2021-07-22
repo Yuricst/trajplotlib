cf053640581b88cdb7943ab6e2692cadc6eed4ee
Yuricst <yuri.shimane@gmail.com>
2021-07-22 23:24:08 +0900

    linecolor function added

--- trajplotlib/__init__.py
+++ trajplotlib/__init__.py
@@ -4,3 +4,4 @@ trajplotlib init file
 
 
 from .helper3d import set_equal_axis, get_sphere_coordinates, plot_sphere_wireframe, plot_ellipsoid_wireframe, get_ellipsoid_coordinates, quickplot3
+from .linecolor import get_lc_traj_singleColor
---
+++ trajplotlib/linecolor.py
@@ -0,0 +1,44 @@
+"""
+Helper for line-color for trajectory plotting
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_lc_traj_singleColor(xs, ys, cs, vmin, vmax, cmap, lw=0.8):
+    """
+    Get line collection object for a trajectory with a single color based on a colormap defined by vmin ~ vmax
+
+    For plotting, run:
+        line = ax.add_collection(lc)
+        fig.colorbar(line, ax=ax, label="Colorbar label")
+
+    Args:
+        xs (np.array): array-like object of x-coordinates of the trajectory
+        ys (np.array): array-like object of y-coordinates of the trajectory
+        cs (float or np.array): float or array-like object of color-values along the coordinates
+        vmin (float): minimum bound on colorbar
+        vmax (float): maximum bound on colorbar
+        cmap (str): colormap, e.g. 'viridis'
+        lw (float): linewidth of trajectory
+
+    Returns:
+        (obj): line collection object
+    """
+    # check if cs is a float, and if it is convert it to an array
+    if isinstance(cs, Real) == True:
+    	cs = cs * np.ones((len(xs),))
+
+    # generate segments
+    points = np.array([ xs , ys ]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    plt_color = cs
+
+    # create color bar
+    norm = plt.Normalize( vmin, vmax )
+    lc = LineCollection(segments, cmap=cmap, norm=norm)
+    # Set the values used for colormapping
+    lc.set_array( plt_color )
+    lc.set_linewidth( lw )
+    return lc
